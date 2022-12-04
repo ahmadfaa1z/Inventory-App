812a9a18820efe06d33f0e0d608532767ebe1915
Ahmad Faaiz <ahmadfaaiz6@gmail.com>
2022-12-04 12:05:51 +0800

    Delete create_db.py

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from db import db
-from app import app, db_filename
-import os
-
-if __name__ == '__main__':
-    if os.path.exists(f'./instance/{db_filename}'):
-        os.remove(f'./instance/{db_filename}')
-    with app.app_context():
-        db.create_all()
